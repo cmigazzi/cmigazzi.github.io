3883f8edeeeeb916fde0d663458cb4cda0c07b9b
cmigazzi <cedric.migazzi@hotmail.fr>
2019-08-01 12:11:35 +0200

    Add melomnia and new theme with modals.

--- theme/js/main.js
+++ theme/js/main.js
@@ -11,21 +11,3 @@ function toHome() {
     portfolio.replaceWith(home)
     portfolio.style.display = "none";
 }
-
-function toContent(target) {
-    var content = document.getElementById(target+'-content')
-    var title = document.getElementById(target+'-title')
-    var wrapper = document.getElementById(target+'-wrapper')
-    if (content.classList.contains("content")) {
-        wrapper.classList.add("col-12");
-        content.classList.replace("content", "content-visible");
-        title.classList.replace("preview", "preview-active");
-
-    } else {
-        content.classList.replace("content-visible", "content");
-        title.classList.replace("preview-active", "preview");
-        wrapper.classList.remove("col-12");
-
-    }
-
-}
